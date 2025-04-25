30cdc1d3147da8a1afcbdefa7b90c6efc145f03f
Ellie <ellie@ellie.au>
2025-04-25 17:43:36 +0930

    Add setting of keybind

--- Mods/Keybinds/ActionUI.gd
+++ Mods/Keybinds/ActionUI.gd
@@ -13,23 +13,23 @@ func set_item(item : Dictionary) -> void:
 	var action : String = item["action_name"]
 	if item["key"] != -1:
 		var key_code : Key = item["key"]
-		#var has_mod : bool  = item["modifier"]
-		#var mod_mask : int = item["modifier_mask"]
-		#
-		var input_event = InputEventKey.new()
-		input_event.physical_keycode = key_code
-		#input_event.get_modifiers_mask()
-		#if has_mod:
-			#if mod_mask & KeyModifierMask.KEY_CODE_MASK:
-				#pass
-		var display_bind = OS.get_keycode_string(
-			DisplayServer.keyboard_get_label_from_physical(
-				input_event.get_physical_keycode_with_modifiers()))
-		
-		%KeybindDisplay.text = display_bind
+		_set_key_bind_display(key_code)
 		
 	%ActionNameTxt.text = action
 	emit_signal("on_change_item", ChangeAction.INITIAL, item, {})
+
+func _set_key_bind_display(key_code : Key):
+	var input_event = InputEventKey.new()
+	input_event.physical_keycode = key_code
+	#input_event.get_modifiers_mask()
+	#if has_mod:
+		#if mod_mask & KeyModifierMask.KEY_CODE_MASK:
+			#pass
+	var display_bind = OS.get_keycode_string(
+		DisplayServer.keyboard_get_label_from_physical(
+			input_event.get_physical_keycode_with_modifiers()))
+	
+	%KeybindDisplay.text = display_bind
 
 func blank_item() -> void:
 	ui_item = {
@@ -53,3 +53,18 @@ func _on_reset_btn_pressed() -> void:
 	ui_item["key"] = -1
 	emit_signal("on_change_item", ChangeAction.KEY_BIND, ui_item, old_item)
 	%KeybindDisplay.text = "Empty"
+
+func _input(event : InputEvent):
+	if currently_setting and event is InputEventKey:
+		currently_setting = false
+		%SetBtn.text = "Set"
+		var new_key = event.physical_keycode
+		var old_item = ui_item.duplicate()
+		ui_item["key"] = new_key
+		emit_signal("on_change_item", ChangeAction.KEY_BIND, ui_item, old_item)
+		_set_key_bind_display(new_key)
+		
+func _on_set_btn_pressed() -> void:
+	currently_setting = true
+	if currently_setting:
+		%SetBtn.text = "Press Key"	
